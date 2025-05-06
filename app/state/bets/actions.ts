a030de0c7bb376531d0d2e93063e6997abe4d5af
Jeremy Karlsson <jeremy@barlingshult.se>
2025-05-06 23:10:30 +0200

    omit redudant key in action payload for loadBetSuccess

--- app/state/bets/actions.ts
+++ app/state/bets/actions.ts
@@ -2,5 +2,5 @@ import { createAction } from "@reduxjs/toolkit";
 import type { Bet, RaceInfo } from "./state";
 
 export const loadBetAction = createAction<Bet, "bets/LOAD_BET">("bets/LOAD_BET");
-export const loadBetSuccess = createAction<RaceInfo, "bets/LOAD_BET_SUCCESS">("bets/LOAD_BET_SUCCESS");
+export const loadBetSuccess = createAction<Omit<RaceInfo, 'status'>, "bets/LOAD_BET_SUCCESS">("bets/LOAD_BET_SUCCESS");
 export const loadBetFailure = createAction<Error, "bets/LOAD_BET_FAILURE">("bets/LOAD_BET_FAILURE");
